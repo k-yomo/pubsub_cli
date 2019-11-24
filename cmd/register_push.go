a437fb655a82af6da675d55435a54548a747ad23
k-yomo <kanji.yy@gmail.com>
2019-11-25 03:12:14 +0900

    Make it possible to use for real Google Pub/Sub

--- cmd/register_push.go
+++ cmd/register_push.go
@@ -18,35 +18,40 @@ var registerPushCmd = &cobra.Command{
 	Short: "register Pub/Sub push endpoint",
 	Long:  "register new endpoint for  push http request from Pub/Sub",
 	Args:  cobra.ExactArgs(2),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		topicID := args[0]
-		endpoint := args[1]
-		client, err := util.NewPubSubClient(projectID, emulatorHost)
-		if err != nil {
-			return errors.Wrap(err, "[error]initialize pubsub client failed")
-		}
-		topic, err := client.FindOrCreateTopic(context.Background(), topicID)
-		if err != nil {
-			return errors.Wrapf(err, "[error]find or create topic %s failed")
-		}
-		_, _ = colorstring.Println(fmt.Sprintf("[start] register push endpoint for %s", topic.String()))
-		subscriptionConfig := pubsub.SubscriptionConfig{
-			Topic:            topic,
-			ExpirationPolicy: time.Hour * 24,
-			PushConfig: pubsub.PushConfig{
-				Endpoint:             endpoint,
-				Attributes:           nil,
-				AuthenticationMethod: nil,
-			},
-		}
-		if _, err := client.CreateSubscription(context.Background(), xid.New().String(), subscriptionConfig); err != nil {
-			return errors.Wrapf(err, "register push endpoint for = %s", topic.String())
-		}
-		_, _ = colorstring.Println(fmt.Sprintf("[green][success] registered %s as an endpoint for %s", endpoint, topic.String()))
-		return nil
-	},
+	RunE:  registerPush,
 }
 
 func init() {
 	rootCmd.AddCommand(registerPushCmd)
+}
+
+func registerPush(_ *cobra.Command, args []string) error {
+	ctx := context.Background()
+	topicID := args[0]
+	endpoint := args[1]
+
+	client, err := util.NewPubSubClient(ctx, projectID, emulatorHost, gcpCredentialFilePath)
+	if err != nil {
+		return errors.Wrap(err, "[error]initialize pubsub client")
+	}
+	topic, err := client.FindOrCreateTopic(ctx, topicID)
+	if err != nil {
+		return errors.Wrapf(err, "[error]find or create topic %s")
+	}
+
+	_, _ = colorstring.Println(fmt.Sprintf("[start] registering push endpoint for %s...", topic.String()))
+	subscriptionConfig := pubsub.SubscriptionConfig{
+		Topic:            topic,
+		ExpirationPolicy: time.Hour * 24,
+		PushConfig: pubsub.PushConfig{
+			Endpoint:             endpoint,
+			Attributes:           nil,
+			AuthenticationMethod: nil,
+		},
+	}
+	if _, err := client.CreateSubscription(context.Background(), xid.New().String(), subscriptionConfig); err != nil {
+		return errors.Wrapf(err, "register push endpoint for = %s", topic.String())
+	}
+	_, _ = colorstring.Println(fmt.Sprintf("[green][success] registered %s as an endpoint for %s", endpoint, topic.String()))
+	return nil
 }
